0a5b777e39d7bc5bfe3400b2086299c0f99f80a9
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-01 15:15:13 -0400

    Initialized Registers
    Created variables for all non-temporary registers to be accessed by entire simulation

--- main.c
+++ main.c
@@ -1,4 +1,28 @@
+/*
+MySPIM Project
+CDA 3103C - Computer Logic & Organization
+
+Contributors:
+Brandon Magloire
+Noah Carrier
+Evan Agustin Verdu
+*/
+
+
 #include <stdio.h>
+
+
+//Global Variables for registers (excluding temporary)
+int a0, a1, a2, a3;                 //Arguments      *
+int s0, s1, s2, s3, s4, s5, s6, s7; //Callees        *
+int zero = 0b0;                     //Zero register  *
+int v0, v1;                         //Funct results  *
+int gp;                             //Global pointer *
+int sp;                             //Stack pointer  *
+int fp;                             //Frame pointer  *
+int ra;                             //Return address *
+//*****************************************************
+
 
 int main()
 {
