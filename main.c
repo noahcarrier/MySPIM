3fbf058fef60e3ecd6eb80538e3276bb960ad90a
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-01 22:09:04 -0400

    Created function declarations
    Added required functions, required structure, and created blank void versions of 'to-be-implemented' functions for later development

--- main.c
+++ main.c
@@ -23,7 +23,44 @@ int fp;                             //Frame pointer  *
 int ra;                             //Return address *
 //*****************************************************
 
+typedef struct
+{
+  char RegDst;
+  char Jump;
+  char Branch;
+  char MemRead;
+  char MemtoReg;
+  char ALUOp;
+  char MemWrite;
+  char ALUSrc;
+  char RegWrite;
+}struct_controls;
 
+
+
+
+
+
+
+//Initalizing functions
+void printReg();             //Print all register contents
+void printMem();             //Print all memory contents
+void step();                 //Step forward 1 instruction
+void contUntilHalt();        //Continue all instructions until halt
+void checkHalt();            //Check if program has halted
+int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction);
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec);
+int instruction_decode(unsigned op, struct_controls *controls);
+void read_register(unsigned r1, unsigned r2, unsigned *Reg, unsigned *data1, unsigned *data2);
+void sign_extend(unsigned offset, unsigned *extended_value);
+int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsigned funct, char ALUOp, char ALUSrc, unsigned *ALUresult, char *Zero);
+int rw_memory(unsigned ALUresult, unsigned data2, char MemWrite, char MemRead, unsigned *memdata, unsigned *Mem);
+void write_register(unsigned r2, unsigned r3, unsigned memdata, unsigned ALUresult, char RegWrite, char RegDst, char MemtoReg, unsigned *Reg);
+void PC_update(unsigned jsec, unsigned extended_value, char Branch, char Jump, char Zero, unsigned *PC);
+//*************************
+
+
+//MAIN FUNCTION
 int main()
 {
   printf("Hello, World!");
