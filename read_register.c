c11f38d32ff514e6cdb3bf46edd379eb203c20de
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-06 23:07:57 -0400

    Update read_register.c

--- read_register.c
+++ read_register.c
@@ -2,12 +2,15 @@
 
 
 /*
-Dictionary
-r1
-r2
-Reg
-data1
-data2
+    Dictionary
+   ***************************************
+   *    r1 -> target
+   *    r2 -> reading value
+   *    Reg -> register array to be read
+   *    data1 -> array of some values
+   *    data2 -> array of some values
+   ****************************************
+
 */
 
 
@@ -22,5 +25,11 @@ int main()
 
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-    
+    for (int index = 0; index < 32; index++)
+    {
+        if (Reg[index] == data1[r1])
+        {
+            data2[r2] = Reg[index];
+        }
+    }
 }
