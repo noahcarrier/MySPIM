f39b4d8985da1810e70430cf2abfa34a0036f49d
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-06 23:31:18 -0400

    Create ALU_operations.c

---
+++ ALU_operations.c
@@ -0,0 +1,8 @@
+
+
+
+
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+   // void ALU( A, B, ALUControl, *ALUresult, *Zero);
+}
