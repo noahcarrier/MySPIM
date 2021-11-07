404d975e2500cf265f430c78a86daaca02bf9344
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-06 23:31:24 -0400

    Update write_register.c

--- write_register.c
+++ write_register.c
@@ -4,14 +4,18 @@
 /*
     Dictionary
    ***************************************
-   *    r2 -> 
-   *    r3 -> 
+   *    r2 -> source
+   *    r3 -> destination
    *    Reg -> register array to be read
-   *    memdata -> array of some values
-   *    ALUresult -> 
-   *    RegWrite ->
-   *    RegDst  ->
-   *    MemtoReg ->
+   *    memdata -> data read from memory
+   *    ALUresult -> ALU output
+   *    RegWrite -> control to write register
+   *    RegDst  ->  destination of register
+   *    MemtoReg -> control indicating data from memory
+   * 
+   * If RegWrite == 1, and MemtoReg == 1, then data is coming from memory
+   * If RegWrite == 1, and MemtoReg == 0, then data is coming from ALU_result
+   * If RegWrite == 1, place write data into the register specified by RegDst
    ****************************************
 
 */
@@ -29,5 +33,9 @@ int main()
 
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+    if (RegWrite == "1" && MemtoReg == "1") //Data coming from memory
+        Reg[RegDst] = memdata;
     
+    if (RegWrite == "1" && MemtoReg == "0") //Data coming from ALU_result
+        Reg[RegDst] = ALUresult;
 }
