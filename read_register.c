2b25786a496bef3bae934a9bae3bf7f1a4e5abb3
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-06 22:42:18 -0400

    Create read_register.c

---
+++ read_register.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/*
+Dictionary
+r1
+r2
+Reg
+data1
+data2
+*/
+
+
+
+int main()
+{
+    unsigned r1, r2, *Reg, *data1, *data2;
+    read_register(r1, r2, *Reg, *data1, *data2);
+    return 0;
+}
+
+
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+    
+}
