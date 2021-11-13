edb88cac45aa86123c74d2731a5c8e86976310fe
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-11-13 16:30:12 -0500

    Update sign_extend.c

--- sign_extend.c
+++ sign_extend.c
@@ -12,8 +12,31 @@ int main()
 
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
+    
+   int first;
 
-//Figure out how to extend offset. Unless it is already extended.
+    first = offset;
+    
+    while (first >= 10) // Finds the first number of offset
+    {
+        first = first / 10;
+    }    
+    
+    if (first == 1)
+    {
+         //Negative value
+         //add 2 bit strings (1111 1111 1111 1111 0000 0000 0000 0000 and offset)
+        
+        
+    }
+    else if (first == 0)
+    {
+          //Positive value
+         //add 2 bit strings (0000 0000 0000 0000 0000 0000 0000 0000 and offset)
+    } 
+    
+
+      
 
 *extended_value = offset;
 }
