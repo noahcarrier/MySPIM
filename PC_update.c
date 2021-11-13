8865dc11443d5870ee1aa0735f3aed67c0a6db6c
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-11-13 17:36:30 -0500

    Update PC_update.c

---
+++ PC_update.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() 
+{
+unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC;
+void PC_update( jsec, extended_value, Branch, Jump, Zero, &PC);
+return 0;
+}
+
+
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
