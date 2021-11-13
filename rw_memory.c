0f1e096bf0f07385570ab313a1da37a1d49ae87a
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-11-13 17:32:27 -0500

    Update rw_memory.c

--- rw_memory.c
+++ rw_memory.c
@@ -4,7 +4,16 @@
 
 int main()
 {
-unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem;
-rw_memory(ALUresult, data2, MemWrite, MemRead, &memdata, &mem);
-return 0;
+   int value;
+   unsigned ALUresult, unsigned data2, char MemWrite, char MemRead, unsigned *memdata, unsigned *Mem;
+   int = rw_memory(ALUresult, data2, MemWrite, MemRead, &memdata, &mem);
+   return 0;
+}
+
+
+int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+  
+  
 }
