8e06d189f8eaf2acbdc4b4feb51b4297d471c23c
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-11-14 18:25:31 -0500

    Update sign_extend.c

--- sign_extend.c
+++ sign_extend.c
@@ -15,18 +15,20 @@ void sign_extend(unsigned offset,unsigned *extended_value)
     
     int sign_bit;
     
-    sign_bit = (offset << 31);
+    sign_bit = (offset >> 15);
     
     if (sign_bit == 1)
     {
-         offset = 0xFFFF0000 + offset;    
+         offset = 0xFFFF0000 + offset;   
+        *extended_value = offset;
     }
     
     else if (sign_bit == 0)
     {
 
          offset = 0x00000000 + offset;
+        *extended_value = offset;
     } 
     
-*extended_value = offset;
+
 }
