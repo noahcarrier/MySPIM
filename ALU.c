9732db8da523cb03c51b167fc53d6969289cd771
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-17 14:16:25 -0500

    Updates to ALU & ALU_operations
    added cases for opcode and funct code

--- ALU.c
+++ ALU.c
@@ -1,23 +1,81 @@
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-    switch (ALUControl)
+   switch (ALUControl)
    {
-      case 0:
-         // Z = A + B
-      case 0b1:
-         // Z = A - B
-      case 0b10:
-         // if A < B, Z = 1, otherwise, Z = 0
-      case 0b11:
-         // if A < B, Z = 1, otherwise Z = 0 (A and B unsigned)
-      case 0b100:
-         // Z = A AND B
-      case 0b101:
-         // Z = A OR B
-      case 0b110:
-         // Z = Shift B left by 16 bits
-      case 0b111:
-         // Z = NOT A
-      default:
+      case 0b000000:
+         ALUresult = A<<B;
+         break;
+      case 0b000001:
+         break;
+      case 0b000010:
+         break;
+      case 0b000011:
+         ALUresult = A * B;
+         break;
+      case 0b000100:
+         ALUresult = A + B;
+         break;
+      case 0b001001:
+         break;
+      case 0b001010:
+         break;
+      case 0b001011:
+         ALUresult = A * B;
+         break;
+      case 0b001100:
+         ALUresult = A + B;
+         break;
+      case 0b010000:
+         ALUresult = A >> B;
+         break;
+      case 0b010010:
+         break;
+      case 0b010011:
+         ALUresult = A / B;
+         break;
+      case 0b010100:
+         ALUresult = A - B;
+         break;
+      case 0b010101:
+         if (A < B)
+            ALUresult = 1;
+         else
+            ALUresult = 0;
+         break;
+      case 0b011000:
+         break;
+      case 0b011010:
+         break;
+      case 0b011011:
+         ALUresult = A / B;
+         break;
+      case 0b011100:
+         ALUresult = A - B;
+         break;
+      case 0b011101:
+         if (A < B)
+            ALUresult = 1;
+         else
+            ALUresult = 0;
+         break;
+      case 0b100000:
+         break;
+      case 0b100001:
+         break;
+      case 0b100100:
+         ALUresult = A & B;
+         break;
+      case 0b101001:
+         break;
+      case 0b101100:
+         ALUresult = A | B;
+         break;
+      case 0b110100:
+         ALUresult = A ^ B;
+      case 0b111000:
+         break;
+      case 0b111100:
+         ALUresult = !(A | B);
+         break;
    }
 }
--- ALU_operations.c
+++ ALU_operations.c
@@ -11,29 +11,52 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 
    switch (ALUOp)
    {
-      case 0:
-         //ALU will do addition or "don't care"
-      case 0b1:
-         //ALU will do subtraction
-      case 0b10:
-         //ALU will do "set less than" operation
-      case 0b11:
+      case 0b000000:
+         ALU(data1, data2, funct, ALUresult, Zero);
+         break;
+      case 0b000001:
+         //addi
+      case 0b000010:
+         //TLB
+      case 0b000011:
          //ALU will do "set less than unsigned" operation
-      case 0b100:
-         //ALU will do "AND" operation
-      case 0b101:
+      case 0b000101:
          //ALU will do "OR" operation
-      case 0b110:
-         //ALU shift left extended_value by 16 bits
-      case 0b111:
-         //Instruction is R-type
-            //Look at funct
-      default:
+      case 0b000110:
+         extended_value<<16;  //ALU shift left extended_value by 16 bits
+         break;
+      case 0b001000:
+         //bltz/gez
+      case 0b001001:
+         //addiu
+      case 0b001010:
+         //FlPt
+      case 0b010001:
+         //Set less than imm
+      case 0b011001:
+         //sltiu
+      case 0b100001:
+         //andi
+      case 0b100100:
+         //lbu
+      case 0b101001:
+         //ori
+      case 0b101100:
+         //lhu
+      case 0b110000:
+         //blez
+      case 0b110001:
+         //xori
+      case 0b111000:
+         //bgtz
    }
    
    
-   void ALU(data1, data2, ALUControl, ALUresult, Zero);
+   ALU(data1, data2, funct, ALUresult, Zero);
 
+   if (ALUresult == 0)
+      Zero = 1;
+   
    if (Halt == 1)
       return 1;
    else
