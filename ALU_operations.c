e24b114ad41750d7223317e7ea972b9d2c23326e
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-18 17:01:35 -0500

    Update ALU_operations.c
    added and refined switch cases for op codes ensuring proper use of halt flag for illegal actions

--- ALU_operations.c
+++ ALU_operations.c
@@ -1,64 +1,64 @@
-static int Halt = 0;
 
 
 
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-
+   static int halt_flag = 0;
+   char ALUControl;
    
-   //Set parameters for A, B, and ALUControl 
-
-
-   switch (ALUOp)
+   switch(ALUOp)
    {
-      case 0b000000:
-         ALU(data1, data2, funct, ALUresult, Zero);
+      case 0b000:
+         ALUControl = 0b000;   //addition or don't care
          break;
-      case 0b000001:
-         //addi
-      case 0b000010:
-         //TLB
-      case 0b000011:
-         //ALU will do "set less than unsigned" operation
-      case 0b000101:
-         //ALU will do "OR" operation
-      case 0b000110:
-         extended_value<<16;  //ALU shift left extended_value by 16 bits
+      case 0b001:
+         ALUControl = 0b001;  //sub
          break;
-      case 0b001000:
-         //bltz/gez
-      case 0b001001:
-         //addiu
-      case 0b001010:
-         //FlPt
-      case 0b010001:
-         //Set less than imm
-      case 0b011001:
-         //sltiu
-      case 0b100001:
-         //andi
-      case 0b100100:
-         //lbu
-      case 0b101001:
-         //ori
-      case 0b101100:
-         //lhu
-      case 0b110000:
-         //blez
-      case 0b110001:
-         //xori
-      case 0b111000:
-         //bgtz
+      case 0b0010:
+         ALUControl = 0b010;   //slt
+         break;
+      case 0b011:
+         ALUresult = 0b011;   //sltu
+         break;
+      case 0b100:
+         ALUControl = 0b100;  //and
+         break;
+      case 0b101:
+         ALUControl = 0b101;  //or
+         break;
+      case 0b110:
+         ALUControl = 0b110;  //shift extend left 16
+         break;
+      case 0b111:
+         //R-type, checking fucnct
+         switch(funct)
+         {
+            case 0b000011:    //mult
+               ALUControl = 0b1000;
+               break;
+            case 0b010011:    //div
+               ALUControl = 0b100;
+               break;
+            case 0b000100:    //add
+               ALUControl = 0b000;
+               break;
+            case 0b010100:    //sub
+               ALUControl = 0b001;
+               break;
+         }
+      default:
+         halt_flag = 1;
    }
-   
-   
-   ALU(data1, data2, funct, ALUresult, Zero);
 
-   if (ALUresult == 0)
-      Zero = 1;
+   if (ALUSrc == 0)
+      ALU(data1, data2, ALUControl, &ALUresult, &Zero);
+   if (ALUSrc == 1)
+      ALU(data1, extended_value, ALUControl, &ALUresult, &Zero);
    
-   if (Halt == 1)
-      return 1;
-   else
-      return 0;
+      if (halt_flag == 1)
+         return 1;
+      else
+         return 0;
 }
+
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero);
