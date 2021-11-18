d16ee7375bf28be12a994591c3df1a3636168e6d
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2021-11-18 17:02:05 -0500

    Update ALU.c
    completed set of cases including adding two extra for r-type instructions: multiply and divide

--- ALU.c
+++ ALU.c
@@ -1,81 +1,47 @@
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-   switch (ALUControl)
-   {
-      case 0b000000:
-         ALUresult = A<<B;
-         break;
-      case 0b000001:
-         break;
-      case 0b000010:
-         break;
-      case 0b000011:
-         ALUresult = A * B;
-         break;
-      case 0b000100:
+
+  switch (ALUControl)
+  {
+      case 0b000:
          ALUresult = A + B;
          break;
-      case 0b001001:
-         break;
-      case 0b001010:
-         break;
-      case 0b001011:
-         ALUresult = A * B;
-         break;
-      case 0b001100:
-         ALUresult = A + B;
-         break;
-      case 0b010000:
-         ALUresult = A >> B;
-         break;
-      case 0b010010:
-         break;
-      case 0b010011:
-         ALUresult = A / B;
-         break;
-      case 0b010100:
+      case 0b001:
          ALUresult = A - B;
          break;
-      case 0b010101:
+      case 0b010:
          if (A < B)
             ALUresult = 1;
          else
             ALUresult = 0;
          break;
-      case 0b011000:
-         break;
-      case 0b011010:
-         break;
-      case 0b011011:
-         ALUresult = A / B;
-         break;
-      case 0b011100:
-         ALUresult = A - B;
-         break;
-      case 0b011101:
+      case 0b011:
          if (A < B)
             ALUresult = 1;
          else
             ALUresult = 0;
          break;
-      case 0b100000:
-         break;
-      case 0b100001:
-         break;
-      case 0b100100:
+      case 0b100:
          ALUresult = A & B;
          break;
-      case 0b101001:
-         break;
-      case 0b101100:
+      case 0b101:
          ALUresult = A | B;
          break;
-      case 0b110100:
-         ALUresult = A ^ B;
-      case 0b111000:
+      case 0b110:
+         ALUresult = B << 16;
          break;
-      case 0b111100:
-         ALUresult = !(A | B);
+      case 0b111:
+         ALUresult = !A;
          break;
-   }
+      case 0b1000:
+         ALUresult = A * B;
+         break;
+      case 0b1001:
+         ALUresult = A / B;
+         break;
+      default:
+      //   halt_flag = 1;
+  }
+   if (ALUresult == 0)
+      Zero = 1;
 }
